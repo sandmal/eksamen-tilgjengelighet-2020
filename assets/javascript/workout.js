c184a46e581b1838e7ed121d9711c2b99775412d
Cornelius <sandmal.cornelius@gmail.com>
2020-11-20 11:54:40 +0100

    Added modal for mobile and pc

--- assets/javascript/workout.js
+++ assets/javascript/workout.js
@@ -1,1 +1,38 @@
-const formDiv = document.queryselector(".selectExcercise");
+// const formDiv = document.querySelector(".selectWorkout");
+// const button = document.querySelector("#btn");
+// const arms = document.querySelectorAll("[data-class='arms']");
+// const jump = document.querySelectorAll("[data-class='jump']");
+
+// let select = document.querySelector("#muscleGroup");
+
+// console.log(select.value);
+// console.log(jump);
+
+// button.addEventListener("click", function () {
+//   if (select.value == "arms") {
+//     arms.forEach((item) => {
+//       item.classList.toggle("active");
+//     });
+//   } else if (select.value == "jump") {
+//     jump.forEach((item) => {
+//       item.classList.toggle("active");
+//     });
+//   }
+// });
+
+const modal = document.getElementById("selectWorkoutModal");
+const btn = document.querySelector("#workout>button");
+const span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
