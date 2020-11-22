6949d1022ddeff3a90a38ad0f18128ab2abd912d
Cornelius <sandmal.cornelius@gmail.com>
2020-11-22 23:15:17 +0100

    js fix

--- assets/javascript/workout.js
+++ assets/javascript/workout.js
@@ -1,24 +1,5 @@
-// const formDiv = document.querySelector(".selectWorkout");
-// const button = document.querySelector("#btn");
 const arms = document.querySelectorAll("[data-class='arms']");
-// const jump = document.querySelectorAll("[data-class='jump']");
-
-// let select = document.querySelector("#muscleGroup");
-
-// console.log(select.value);
-// console.log(jump);
-
-// button.addEventListener("click", function () {
-//   if (select.value == "arms") {
-//     arms.forEach((item) => {
-//       item.classList.toggle("active");
-//     });
-//   } else if (select.value == "jump") {
-//     jump.forEach((item) => {
-//       item.classList.toggle("active");
-//     });
-//   }
-// });
+const jump = document.querySelectorAll("[data-class='jump']");
 
 const modal = document.getElementById("selectWorkoutModal");
 const btn = document.querySelector("#workout>button");
@@ -26,7 +7,8 @@ const close = document.getElementsByClassName("close")[0];
 const closeBtn = document.getElementsByClassName("closeBtn")[0];
 
 const form = document.querySelector("form");
-// const optionValue = document.querySelector("input");
+const inputChecked = document.querySelector("input").id;
+
 btn.onclick = function () {
   modal.style.display = "block";
 };
@@ -46,9 +28,49 @@ window.onclick = function (event) {
 };
 
 form.addEventListener("submit", function (event) {
-  // console.log(optionValue.value);
-  arms.forEach((item) => {
-    console.log(item);
-  });
   event.preventDefault();
+  if (inputChecked == "arms") {
+    console.log("tes");
+  }
 });
+let running = false;
+
+var stop;
+let excercises = 3;
+const startingElements = excercises;
+let time = startingElements * 10;
+const countdownEl = document.getElementById("countdown");
+
+function workout() {
+  // Hele workout
+  //Kjør en excercise
+  //vent til en excercise er over
+  //lag en alert som sier pause i x sekunder
+  //Repeat
+
+  if (running == false) {
+    running = true;
+    stop = setInterval(excercise, 1000);
+    document.getElementById("startResume").innerHTML = "Pause";
+  } else {
+    running = false;
+    clearInterval(stop);
+    document.getElementById("startResume").innerHTML = "Resume";
+  }
+}
+
+//kjører når en excercise blir kalt
+function excercise() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  countdownEl.innerHTML = `${minutes}:${seconds}`;
+  if ((running = true)) {
+    time--;
+  }
+  time = time < 0 ? 0 : time;
+}
+
+function takeBreak() {
+  alert("Take a break");
+}
